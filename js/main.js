81f82b57e0a6cd76c0f903a2f6a43b79eb6da2d2
andrei_labadaev <53191183+ivolat05@users.noreply.github.com>
2021-08-01 21:20:54 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(function () {
+    $('.menu__burger').click((event) => {
+        $('.menu__burger').toggleClass('burger__active'),
+            $('.menu').toggleClass('menu__active'),
+            $('.header__wrapp-inner').toggleClass('header__wrapp-inner__active'),
+            $('body').toggleClass('look')
+    });
+
+    $(".main__box").mCustomScrollbar({
+        axis: "x",
+        autoHideScrollbar: true
+    });
+
+    $(".insta__wrapp").slick({
+        arrows: false,
+        slidesToShow: 6,
+        slidesToScroll: 1
+    });
+});
